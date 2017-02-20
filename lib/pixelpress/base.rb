31cad7212e0976a60507ddcbf88396f8055a682b
Michael Sievers <msievers@users.noreply.github.com>
2017-02-20 16:00:20 +0100

    Retabbed base.rb

--- lib/pixelpress/base.rb
+++ lib/pixelpress/base.rb
@@ -1,53 +1,53 @@
 require "action_controller"
 
 module PixelPress
-	class Base < ActionController::Base
-		def html
-		  render(template) unless @html
-		  @html
-		end
+  class Base < ActionController::Base
+    def html
+      render(template) unless @html
+      @html
+    end
 
-		def pdf
-		  stream = StringIO.new pdf_data
-		  stream.class.class_eval { attr_accessor :original_filename, :content_type }
-		  stream.original_filename = try(:file_name) || "document.pdf"
-		  stream.content_type = "application/pdf"
-		  stream
-		end
+    def pdf
+      stream = StringIO.new pdf_data
+      stream.class.class_eval { attr_accessor :original_filename, :content_type }
+      stream.original_filename = try(:file_name) || "document.pdf"
+      stream.content_type = "application/pdf"
+      stream
+    end
 
-		class << self
-		  def method_missing(m, *args, &block)
-		    return super unless respond_to_missing?(m)
-		    instance = new
-		    instance.send(m, *args)
-		    instance.instance_variable_set :@template_name, m.to_s
-		    instance
-		  end
+    class << self
+      def method_missing(m, *args, &block)
+        return super unless respond_to_missing?(m)
+        instance = new
+        instance.send(m, *args)
+        instance.instance_variable_set :@template_name, m.to_s
+        instance
+      end
 
-		  def respond_to_missing?(m, include_private = false)
-		    return true if new.methods.include?(m)
-		    super(m, include_private)
-		  end
-		end
+      def respond_to_missing?(m, include_private = false)
+        return true if new.methods.include?(m)
+        super(m, include_private)
+      end
+    end
 
-		protected
+    protected
 
-		attr_accessor :template
+    attr_accessor :template
 
-		def template
-		  ["printers", self.class.name.to_s.underscore, @template_name].join("/")
-		end
+    def template
+      ["printers", self.class.name.to_s.underscore, @template_name].join("/")
+    end
 
-		private
+    private
 
-		def pdf_data
-		  return "%PDF-1.5" if Rails.env.test?
-		  WeasyPrint.new(html).to_pdf
-		end
+    def pdf_data
+      return "%PDF-1.5" if Rails.env.test?
+      WeasyPrint.new(html).to_pdf
+    end
 
-		def render(template = caller_locations(1, 1)[0].label)
-		  @html = render_to_string(template, layout: "pdf")
-		end
-	end
+    def render(template = caller_locations(1, 1)[0].label)
+      @html = render_to_string(template, layout: "pdf")
+    end
+  end
 
 end
