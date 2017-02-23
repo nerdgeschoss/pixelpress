5e813ffe01b7e4ec0af898748cc7590a8ec5cbd1
Alex <aleksandra@nerdgeschoss.de>
2017-02-23 16:17:34 +0100

    custom generator creating methods passed in command line

--- lib/generators/pixelpress/printer/printer_generator.rb
+++ lib/generators/pixelpress/printer/printer_generator.rb
@@ -3,9 +3,12 @@ module Pixelpress
     class PrinterGenerator < ::Rails::Generators::NamedBase
       source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
 
+      argument :passed_methods, type: :array, default: [],  required: false, banner: "method_name1 method_name2 ..."
+
       def create_custom_printer
         template 'printer.rb', File.join('app/printers', class_path, "#{file_name}_printer.rb")
       end
+
     end
   end
 end
--- lib/generators/pixelpress/printer/templates/printer.rb
+++ lib/generators/pixelpress/printer/templates/printer.rb
@@ -1,5 +1,10 @@
 <% module_namespacing do -%>
 class <%= class_name %>Printer < ApplicationPrinter
-  	
+
+  <% passed_methods.each do |m| %>
+  def <%= m %>
+    #put your code here, if you want :)
+  end  	
+  <% end %>
 end
 <% end -%>
