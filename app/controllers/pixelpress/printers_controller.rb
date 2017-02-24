798831aafcb3b5a6c36e2df5cd44461bf0400a89
Jens Ravens <jens@nerdgeschoss.de>
2017-02-24 15:14:22 +0100

    implement previews via engine as in #2

---
+++ app/controllers/pixelpress/printers_controller.rb
@@ -0,0 +1,15 @@
+class Pixelpress::PrintersController < ActionController::Base
+  def index
+    @printers = Pixelpress::Preview.all
+  end
+
+  def show
+    klass = params[:printer_id]
+    method = params[:id]
+    printer = Pixelpress::Preview.all.map { |e| [e.printer_name, e] }.to_h[klass].send(method)
+    respond_to do |format|
+      format.html { render html: printer.html }
+      format.pdf { send_data printer.pdf.read, disposition: "inline", type: "application/pdf" }
+    end
+  end
+end
