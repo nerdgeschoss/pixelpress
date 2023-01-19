90f76558e99eba4df44a4475b825d93d6e8d383b
Adeynack <Adeynack@users.noreply.github.com>
2023-01-19 20:11:43 +0100

    split pos. & k.w. args

--- lib/pixelpress/instance_invocation.rb
+++ lib/pixelpress/instance_invocation.rb
@@ -1,10 +1,10 @@
 module Pixelpress
   module InstanceInvocation
-    def method_missing(m, *args, &block)
+    def method_missing(m, *args, **kwargs, &block)
       return super unless respond_to_missing?(m)
       instance = new
       instance.instance_variable_set :@template_name, m.to_s
-      instance.send(m, *args)
+      instance.send(m, *args, **kwargs, &block)
       instance.document
     end
 
