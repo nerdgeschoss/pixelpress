95379097ed0fd35fe4264b81ba404eb0c1621155
Jens Ravens <jens@nerdgeschoss.de>
2023-10-25 14:50:49 +0200

    reimplement weasyprint without dependency

--- lib/pixelpress/renderers/weasyprint_renderer.rb
+++ lib/pixelpress/renderers/weasyprint_renderer.rb
@@ -1,7 +1,26 @@
-require 'weasyprint'
-
 class Pixelpress::WeasyPrintRenderer
   def render(html)
-    WeasyPrint.new(html).to_pdf
+
+    command = [executable_path, "-", "-"].shelljoin
+
+    result = IO.popen(command, "wb+") do |pdf|
+      pdf.puts(html)
+      pdf.close_write
+      pdf.gets(nil)
+    end
+
+    # $? is thread safe per http://stackoverflow.com/questions/2164887/thread-safe-external-process-in-ruby-plus-checking-exitstatus
+    raise "command failed (exitstatus=#{$?.exitstatus}): #{command}" if result.to_s.strip.empty? || !$?.success?
+    return result
+  end
+  
+  def version
+    `#{executable_path} --version`.chomp.scan(/(\d+\.\d+)/).flatten.first
+  end
+
+  private
+
+  def executable_path
+    `which weasyprint`.chomp || raise(StandardError, "could not find weasyprint")
   end
 end
--- lib/pixelpress/rendering.rb
+++ lib/pixelpress/rendering.rb
@@ -1,28 +1,21 @@
 module Pixelpress
   module Rendering
-    module ClassMethods
-      attr_writer :default_renderer
-      def default_renderer
-        @default_renderer ||= WeasyPrintRenderer.new
+    extend ActiveSupport::Concern
+    
+    included do
+      class_attribute :renderer, default: WeasyPrintRenderer.new
+
+      def document
+        @document ||= Document.new render_to_string(template), renderer, file_name: try(:file_name)
       end
-    end
 
-    def self.included(base)
-      base.extend(ClassMethods)
-    end
+      protected
+      
+      delegate :renderer, to: :class
 
-    def document
-      @document ||= Document.new render_to_string(template), renderer, file_name: try(:file_name)
-    end
-
-    protected
-
-    def renderer
-      @renderer || self.class.default_renderer
-    end
-
-    def template
-      ['printers', controller_path.sub('_printer', ''), @template_name].join('/')
+      def template
+        ['printers', controller_path.sub('_printer', ''), @template_name].join('/')
+      end
     end
   end
 end
