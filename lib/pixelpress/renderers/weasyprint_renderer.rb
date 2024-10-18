0d6d1eb4468e0b627968ca5b5cb205edef703a0a
Daniel Diekmeier <daniel@nerdgeschoss.de>
2024-10-18 15:01:54 +0200

    [Docs] Add note that WeasyPrint package is required (#27)

--- lib/pixelpress/renderers/weasyprint_renderer.rb
+++ lib/pixelpress/renderers/weasyprint_renderer.rb
@@ -1,11 +1,13 @@
 class Pixelpress::WeasyPrintRenderer
+  class WeasyPrintInstallationError < StandardError; end
+
   def render(input)
     output = Tempfile.new
 
     system executable_path, "--encoding", "utf-8", input.path, output.path, exception: true
     return output
   end
-  
+
   def version
     `#{executable_path} --version`.chomp.scan(/(\d+\.\d+)/).flatten.first
   end
@@ -13,6 +15,12 @@ class Pixelpress::WeasyPrintRenderer
   private
 
   def executable_path
-    `which weasyprint`.chomp || raise(StandardError, "could not find weasyprint")
+    path = `which weasyprint`.chomp
+
+    if path.blank?
+      raise WeasyPrintInstallationError.new("Unable to locate weasyprint binary. Please make sure it's in your PATH.")
+    end
+
+    path
   end
 end
